2bff1e8c5a125f5ba6b827375962624c518e55a5
Steve Mackey <stevester94@gmail.com>
2017-10-25 21:27:39 -0700

    Add sublime text

--- deploy.bash
+++ deploy.bash
@@ -1,6 +1,7 @@
 mkdir backups
-mv ~/.vim ~/.vimrc ~/.bashrc backups
+mv ~/.vim ~/.vimrc ~/.bashrc ~/.config/sublime-text-3  backups
 
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/sublime-text-3 ~/.config/sublime-text-3
