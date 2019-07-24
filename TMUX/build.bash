1bd8622c00a5edaddbf3a03ac090aa54d942fcb0
Steven Mackey <stevester94@gmail.com>
2019-07-24 09:34:07 -0700

    tmux build works now

--- TMUX/build.bash
+++ TMUX/build.bash
@@ -1,1 +1,4 @@
-(cd libevent && ./configure --prefix=../ && make -j4 && make install)
+(cd libevent-2.0.22-stable && ./configure --prefix=$PWD/../build/ && make -j4 && make install)
+(cd ncurses && ./configure --prefix=$PWD/../build/ && make -j4 && make install)
+(cd tmux-2.2 && ./configure --prefix=$PWD/../build/ CFLAGS="-I$PWD/../build/include -I$PWD../build/include/ncurses" LDFLAGS="-static -L$PWD/../build/lib" && \
+    make -j4 && make install )
