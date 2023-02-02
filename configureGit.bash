c7c3368a5bd171d168855aa31118c3142cacb0ce
Steven Mackey <stevester94@gmail.com>
2023-02-02 10:28:29 -0800

    Add credential storage option for git

--- configureGit.bash
+++ configureGit.bash
@@ -1,3 +1,26 @@
 #! /bin/bash
 git config --global user.email "stevester94@gmail.com"
 git config --global user.name "Steven Mackey"
+
+echo "Now configuring git credential storage"
+echo "Configure git to store credentials forever in clear text: F"
+echo "Configure git to store credentials for 15 minutes in a secure daemon: D"
+echo "Skip configuration: S"
+
+while true; do
+    read -p "Credential Storage Mode: " ans 
+    if [[ $ans = F ]]; then
+        echo Configuring to store forever
+        git config --global credential.helper store
+        break
+    elif [[ $ans = D ]]; then
+        echo Configuring to store in daemon
+        git config --global credential.helper cache
+        break
+    elif [[ $ans = S ]]; then
+        echo Skipping
+        break
+    else
+        echo "Invalid input"
+    fi  
+done
