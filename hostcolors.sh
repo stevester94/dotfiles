99a1c7282b28eebc311612778652d1ccb4a03d69
Steven Mackey <stevester94@gmail.com>
2026-02-19 05:59:12 +0000

    hostcolors?

--- hostcolors.sh
+++ hostcolors.sh
@@ -1,7 +1,7 @@
 getHostColor() {
 h=`hostname`
     if [ "$h" == penguin ]; then printf $LIGHT_BLUE
-    elif [ "$h" == htpc ]; then printf $BLUE
+    elif [ "$h" == htpc ]; then printf $LIGHT_GREY
     else printf $LIGHT_GREY
     fi
     
